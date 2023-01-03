12a653e0fa8905c558271d0e4a764ab9fa0acf14
Patrick Brennan <patbrennan7@gmail.com>
2023-01-03 15:58:21 -0500

    Snake Game Started

---
+++ Snake-Game/src/GamePanel.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+  GamePanel() {
+    // stuff
+  }
+  
+  public void actionPerformed(ActionEvent e) {
+    // ACTIon!
+  }
+}
