b6268c51ac3dc8c2c53d0d904d1498cb6b1e21fc
Patrick Brennan <patbrennan7@gmail.com>
2023-01-03 16:23:01 -0500

    All instances set up in GamePanel

--- Snake-Game/src/GamePanel.java
+++ Snake-Game/src/GamePanel.java
@@ -1,15 +1,71 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+  static final int SCREEN_WIDTH = 600;
+  static final int SCREEN_HEIGHT = 600;
+  static final int UNIT_SIZE = 25;
+  static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+  static final int DELAY = 75;
+  final int x[] = new int[GAME_UNITS];
+  final int y[] = new int[GAME_UNITS];
+  int bodyParts = 6;
+  int applesEaten;
+  int appleX;
+  int appleY;
+  char direction = 'R';
+  boolean running = false;
+  Timer timer;
+  Random random;
+
   GamePanel() {
-    // stuff
+    random = new Random();
+    this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+    this.setBackground(Color.CYAN);
+    this.setFocusable(true);
+    this.addKeyListener(new MyKeyAdapter());
+    startGame();
   }
-  
+
+  public void startGame() {
+
+  }
+
+  public void paintComponent(Graphics g) {
+
+  }
+
+  public void draw(Graphics g) {
+
+  }
+
+  public void move() {
+
+  }
+
+  public void checkApple() {
+
+  }
+
+  public void checkCollisions() {
+
+  }
+
+  public void gameOver(Graphics g) {
+
+  }
+
   public void actionPerformed(ActionEvent e) {
     // ACTIon!
+  }
+
+  public class MyKeyAdapter extends KeyAdapter {
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
   }
 }
