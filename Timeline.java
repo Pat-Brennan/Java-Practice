5e625caf882f0f3f697e0c392c6131b420e5560b
Patrick Brennan <patbrennan7@gmail.com>
2023-01-03 13:38:39 -0500

    Java Comments Covered

---
+++ Timeline.java
@@ -0,0 +1,11 @@
+public class Timeline {
+  public static void main(String[] args) {
+    System.out.println("There's comments in here!");
+    // It's me! I'm the comment!
+
+    System.out.println("And even multiline comments!");
+    /* It is I, 
+        The dreaded multiline comment!
+    */
+  }
+}
